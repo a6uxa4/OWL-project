031f559e3750d3e0232d8994ffdd2f1faa13cc29
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-09-03 03:52:24 +0600

    add new document hz chto za file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link ref="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>  
+  );
 }
