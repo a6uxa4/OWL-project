d5dad97b808b5e839676600903f5803e976832d0
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-09-10 03:41:37 +0600

    moduel css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home():JSX.Element {
+import { Htag } from "@/components";
+
+export default function Home(): JSX.Element {
   return (
     <div>
-           HELLO
+      <Htag tag="h1">Text</Htag>
+      <Htag tag="h2">Text</Htag>
+      <Htag tag="h3">Text</Htag>
     </div>
   );
 }
