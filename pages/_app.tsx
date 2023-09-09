3e2c0e077e5b9db61a24c0f187323890ad20180b
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-09-10 03:20:25 +0600

    save

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,13 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link ref="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <Component {...pageProps} />;
-    </>  
+      <Component {...pageProps} />
+    </>
   );
 }
