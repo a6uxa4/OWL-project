c96c9dd5c42dbfdabc318444c955be1ee7c5c673
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-09-14 17:07:04 +0600

    save

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css";
+import ArrowIcon from "./arrow.svg";
 import cn from "classnames";
 
-export const Button = ({ appreance, children }: ButtonProps): JSX.Element => {
+export const Button = ({
+  appreance,
+  children,
+  className,
+  arrow = "none",
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className={cn(styles.button, {
-        [styles.primary]: appreance === "primary",
-        [styles.ghost]: appreance === "ghost",
+      className={cn(styles.button, className, {
+        [styles.primary]: appreance == "primary",
+        [styles.ghost]: appreance == "ghost",
       })}
+      {...props}
     >
       {children}
+      {arrow != "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow == "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
     </button>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,12 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Text</Htag>
       <Htag tag="h2">Text</Htag>
       <Htag tag="h3">Text</Htag>
-      <Button  appreance="primary">Button</Button>
-      <Button appreance="ghost">Button</Button>
+      <Button appreance="primary" arrow="right">
+        Button
+      </Button>
+      <Button appreance="ghost" arrow="right">
+        Button
+      </Button>
     </div>
   );
 }
